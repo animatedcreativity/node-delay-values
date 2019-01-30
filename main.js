0f5a2598cbc65d5c39636ed004dd45771f192f67
Glitch (node-delay-values) <none>
2019-01-30 09:21:23 +0000

    🍣🍔 Updated with Glitch

--- main.js
+++ main.js
@@ -5,10 +5,3 @@ module.exports = exports = function(time, values) {
     }, time);
   });
 };
-var delay = exports;
-for (var i=0; i<=9; i++) {  
-  var plus2 = i + 2;
-  delay(2000, {index: i, plus2: plus2}).then(function(values) {
-    console.log(values); // do something with values after 2 seconds.
-  });
-}
